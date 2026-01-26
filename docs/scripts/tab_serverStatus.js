adcd326612350819f00215a31c90f596c41afb20
Emmote <emmote2@gmail.com>
2026-01-26 01:47:49 +0000

    Updating Server Status tab.
    - Increasing timeout to decide if a server is up or down to 15 seconds from 10 seconds.
    - Adding a Slow server indicator (a horizontal yellow-orange bar).

--- docs/scripts/tab_serverStatus.js
+++ docs/scripts/tab_serverStatus.js
@@ -1,6 +1,8 @@
-const vss = 2.000; // prettier-ignore
-const ss_SVG_tick = `<svg width="22" height="22" viewBox="1.5 -9.1 14 14" xmlns="http://www.w3.org/2000/svg" fill="var(--AlienArmpit)" stroke="var(--Black)" stroke-width=".4"><path fill-rule="evenodd" d="m14.75-5.338a1 1 0 0 0-1.5-1.324l-6.435 7.28-3.183-2.593a1 1 0 0 0-1.264 1.55l3.929 3.2a1 1 0 0 0 1.38-.113l7.072-8z"/></svg>`;
-const ss_SVG_cross = `<svg width="22" height="22" viewBox="1 1 34 34" xmlns="http://www.w3.org/2000/svg" stroke="var(--Black)" stroke-width=".8"><path fill="var(--CarminePink)" d="M21.533 18.002 33.768 5.768a2.5 2.5 0 0 0-3.535-3.535L17.998 14.467 5.764 2.233a2.5 2.5 0 0 0-3.535 0 2.5 2.5 0 0 0 0 3.535l12.234 12.234L2.201 30.265a2.498 2.498 0 0 0 1.768 4.267c.64 0 1.28-.244 1.768-.732l12.262-12.263 12.234 12.234a2.5 2.5 0 0 0 1.768.732 2.5 2.5 0 0 0 1.768-4.267z"/></svg>`;
+const vss = 2.001; // prettier-ignore
+const ss_SVG_up = `<svg width="22" height="22" viewBox="1.5 -9.1 14 14" xmlns="http://www.w3.org/2000/svg" fill="var(--AlienArmpit)" stroke="var(--Black)" stroke-width=".4"><path fill-rule="evenodd" d="m14.75-5.338a1 1 0 0 0-1.5-1.324l-6.435 7.28-3.183-2.593a1 1 0 0 0-1.264 1.55l3.929 3.2a1 1 0 0 0 1.38-.113l7.072-8z"/></svg>`;
+const ss_SVG_down = `<svg width="22" height="22" viewBox="1 1 34 34" xmlns="http://www.w3.org/2000/svg" stroke="var(--Black)" stroke-width=".8"><path fill="var(--CarminePink)" d="M21.533 18.002 33.768 5.768a2.5 2.5 0 0 0-3.535-3.535L17.998 14.467 5.764 2.233a2.5 2.5 0 0 0-3.535 0 2.5 2.5 0 0 0 0 3.535l12.234 12.234L2.201 30.265a2.498 2.498 0 0 0 1.768 4.267c.64 0 1.28-.244 1.768-.732l12.262-12.263 12.234 12.234a2.5 2.5 0 0 0 1.768.732 2.5 2.5 0 0 0 1.768-4.267z"/></svg>`;
+const ss_SVG_slow = `<svg width="22" height="22" xmlns="http://www.w3.org/2000/svg" stroke="var(--Black)" stroke-width=".6" fill="var(--Saffron)"><rect height="4" rx="1.5" ry="1.5" width="21" x=".5" y="9"></rect></svg>`;
+const ss_SLOW_THRESHOLD_MS = 6000;
 let ss_ageTimer = null;
 
 async function ss_pullServerStatusData() {
@@ -37,15 +39,27 @@ async function ss_displayServerStatusData(wrapper, statusData) {
 	txt += ss_addServerStatusRow(`Server`, `Status`, `&nbsp;`, true);
 
 	for (const r of results) {
-		const alive = r.up ? ss_SVG_tick : ss_SVG_cross;
+		const slowResponse = r.responseTimeMs >= ss_SLOW_THRESHOLD_MS;
+		const alive =
+			r.up ?
+				slowResponse ? ss_SVG_slow
+				:	ss_SVG_up
+			:	ss_SVG_down;
 
 		const isServerDown = !r.up && r.lastSeenUp;
 		const lastUp =
-			isServerDown ?
+			!isServerDown && slowResponse ? `Slow`
+			: isServerDown ?
 				`Down (last seen up ${buildTimestampSpan(r.lastSeenUp, true, true)} ago)`
 			:	`&nbsp;`;
 
-		txt += ss_addServerStatusRow(`${r.server}:`, alive, lastUp, false, isServerDown);
+		txt += ss_addServerStatusRow(
+			`${r.server}:`,
+			alive,
+			lastUp,
+			false,
+			lastUp !== `&nbsp;`,
+		);
 	}
 
 	setFormsWrapperFormat(wrapper, 4);
