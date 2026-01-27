4cd92356fc39d1a7dd877eab7a91ac3bf3fcb29b
Emmote <emmote2@gmail.com>
2026-01-27 19:29:37 +0000

    Updating Server Status tab.
    - Adding a key with information on timing.

--- docs/scripts/tab_serverStatus.js
+++ docs/scripts/tab_serverStatus.js
@@ -1,4 +1,4 @@
-const vss = 2.004; // prettier-ignore
+const vss = 2.005; // prettier-ignore
 const ss_SVG_up = `<svg width="22" height="22" viewBox="1.5 -9.1 14 14" xmlns="http://www.w3.org/2000/svg" fill="var(--AlienArmpit)" stroke="var(--Black)" stroke-width=".4"><path fill-rule="evenodd" d="m14.75-5.338a1 1 0 0 0-1.5-1.324l-6.435 7.28-3.183-2.593a1 1 0 0 0-1.264 1.55l3.929 3.2a1 1 0 0 0 1.38-.113l7.072-8z"/></svg>`;
 const ss_SVG_down = `<svg width="22" height="22" viewBox="1 1 34 34" xmlns="http://www.w3.org/2000/svg" stroke="var(--Black)" stroke-width=".8"><path fill="var(--CarminePink)" d="M21.533 18.002 33.768 5.768a2.5 2.5 0 0 0-3.535-3.535L17.998 14.467 5.764 2.233a2.5 2.5 0 0 0-3.535 0 2.5 2.5 0 0 0 0 3.535l12.234 12.234L2.201 30.265a2.498 2.498 0 0 0 1.768 4.267c.64 0 1.28-.244 1.768-.732l12.262-12.263 12.234 12.234a2.5 2.5 0 0 0 1.768.732 2.5 2.5 0 0 0 1.768-4.267z"/></svg>`;
 const ss_SVG_slow = `<svg width="22" height="22" xmlns="http://www.w3.org/2000/svg" stroke="var(--Black)" stroke-width=".6" fill="var(--Saffron)"><rect height="4" rx="1.5" ry="1.5" width="21" x=".5" y="9"></rect></svg>`;
@@ -6,6 +6,7 @@ const ss_SLOW_THRESHOLD_MS = 5 * 1000; // 5 seconds
 const ss_MIN_RECHECK_MS = 30 * 1000; // 30 seconds
 const ss_CACHE_INTERVAL_MS = 10 * 60 * 1000; // 10 minutes
 const ss_CACHE_GRACE_MS = 60 * 1000; // 60 seconds
+const ss_TIMEOUT_MS = 15 * 1000; // 15 seconds
 const ss_COOLDOWN_STORAGE_KEY = `scServerStatusCooldown`;
 let ss_ageTimer = null;
 let ss_recheckTimer = null;
@@ -31,12 +32,14 @@ async function ss_displayServerStatusData(wrapper, statusData) {
 	results.sort((a, b) => ss_compare(a.server, b.server));
 
 	let txt = ``;
+	let paddingStyle = ss_decidePadding(true, true);
 
-	txt +=
-		`<span class="f falc fjs" style="grid-column:1 / -1;padding-bottom:8px">` +
+	txt += ss_addSingleServerStatusRow(
 		`Servers were last checked ` +
-		buildTimestampSpan(statusData?.checkedAt || "", true, true) +
-		` ago.</span>`;
+			ss_buildTimestampSpan(statusData?.checkedAt || "", paddingStyle) +
+			` ago.`,
+	);
+	txt += ss_addSingleServerStatusRow("&nbsp;");
 
 	txt += ss_addServerStatusRow(`Server`, `Status`, `&nbsp;`, true);
 
@@ -52,7 +55,7 @@ async function ss_displayServerStatusData(wrapper, statusData) {
 		const lastUp =
 			!isServerDown && slowResponse ? `Slow`
 			: isServerDown ?
-				`Down (last seen up ${buildTimestampSpan(r.lastSeenUp, true, true)} ago)`
+				`Down (last seen up ${ss_buildTimestampSpan(r.lastSeenUp, paddingStyle)} ago)`
 			:	`&nbsp;`;
 
 		txt += ss_addServerStatusRow(
@@ -64,18 +67,41 @@ async function ss_displayServerStatusData(wrapper, statusData) {
 		);
 	}
 
+	style = ss_decidePadding(false, true);
+	const slowTxt = nf(ss_SLOW_THRESHOLD_MS / 1000) + ` seconds`;
+	const downTxt =
+		nf((results.timeoutMs || ss_TIMEOUT_MS) / 1000) + ` seconds`;
+
+	const keys = [
+		`&nbsp;`,
+		`<b>Key:</b>`,
+		`${ss_buildSVGSpan(ss_SVG_up, style)} Healthy servers took less than ${slowTxt} to respond.`,
+		`${ss_buildSVGSpan(ss_SVG_slow, style)} Slow servers took ${slowTxt} or longer to respond.`,
+		`${ss_buildSVGSpan(ss_SVG_down, style)} Down servers did not respond within ${downTxt}.`,
+	];
+	for (const k of keys) txt += ss_addSingleServerStatusRow(k);
+
 	setFormsWrapperFormat(wrapper, 4);
 	wrapper.innerHTML = txt;
 	ss_startAgeTicker(wrapper);
 	ss_applyCooldownFromStatus(statusData);
 }
 
-function buildTimestampSpan(timer, padLeft, padRight) {
+function ss_decidePadding(padLeft, padRight) {
 	let style = ``;
 	if (padLeft) style += `padding-left:4px;`;
 	if (padRight) style += `padding-right:4px;`;
 	if (style) style = ` style="${style}"`;
+	return style;
+}
+
+function ss_buildTimestampSpan(timer, style) {
 	return `<span data-ss-age-iso="${timer}"${style}>-</span>`;
+}
+
+function ss_buildSVGSpan(svg, style) {
+	svg = svg.replaceAll(/(width|height)="22"/gm, `$1="18"`);
+	return `<span${style}>${svg}</span>`;
 }
 
 function ss_compare(a, b) {
@@ -94,6 +120,10 @@ function ss_addServerStatusRow(server, alive, lastUp, header, dimLastUp) {
 		`<span class="f falc fjc"${style}>${alive}</span>` +
 		`<span class="f falc fjs"${lastUpStyle}>${lastUp}</span>`
 	);
+}
+
+function ss_addSingleServerStatusRow(msg) {
+	return `<span class="f falc fjs" style="grid-column:1 / -1">${msg}</span>`;
 }
 
 function ss_getAgeMs(isoString, nowMs) {
