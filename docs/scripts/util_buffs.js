b49feefe41caf2667330f1e9e5ddfbcebf447694
Emmote <emmote2@gmail.com>
2026-01-29 00:42:37 +0000

    Updating scripts.
    - Removing ChestsData tab because it hasn't been used in forever and probably never will again.
    - Updating util_buffs with more info.

--- docs/scripts/util_buffs.js
+++ docs/scripts/util_buffs.js
@@ -1,5 +1,5 @@
-const vb = 1.002; // prettier-ignore
-const b_buffValues = {
+const vb = 2.000; // prettier-ignore
+const b_basics = {
 	1: "Small Potion of Giant's Strength",
 	2: "Medium Potion of Giant's Strength",
 	3: "Large Potion of Giant's Strength",
@@ -12,15 +12,6 @@ const b_buffValues = {
 	14: "Medium Potion of Heroism",
 	15: "Large Potion of Heroism",
 	16: "Huge Potion of Heroism",
-	17: "Tiny Bounty Contract",
-	18: "Small Bounty Contract",
-	19: "Medium Bounty Contract",
-	20: "Large Bounty Contract",
-	31: "Tiny Blacksmithing Contract",
-	32: "Small Blacksmithing Contract",
-	33: "Medium Blacksmithing Contract",
-	34: "Large Blacksmithing Contract",
-	35: "Potion of Specialization",
 	37: "Small Potion of Fire Breath",
 	38: "Medium Potion of Fire Breath",
 	39: "Large Potion of Fire Breath",
@@ -29,5 +20,34 @@ const b_buffValues = {
 	75: "Medium Potion of Speed",
 	76: "Large Potion of Speed",
 	77: "Huge Potion of Speed",
+	2164: "Legendary Potion of Giant's Strength",
+	2165: "Legendary Potion of Clairvoyance",
+	2166: "Legendary Potion of Heroism",
+	2167: "Legendary Fire Breath Potion",
+	2168: "Legendary Potion of Speed",
+};
+const b_pop = {
+	36: "Potion of Polish",
+};
+const b_spec = {
+	35: "Potion of Specialization",
+};
+const b_7days = {
+	1712: "Potion of the Hunter",
+	1721: "Potion of the Gold Hunter",
+	1722: "Potion of Psychomorphic Energy",
+	1723: "Potion of the Gem Hunter",
+};
+const b_blacksmiths = {
+	31: "Tiny Blacksmithing Contract",
+	32: "Small Blacksmithing Contract",
+	33: "Medium Blacksmithing Contract",
+	34: "Large Blacksmithing Contract",
 	1797: "Huge Blacksmithing Contract",
+};
+const b_bounties = {
+	17: "Tiny Bounty Contract",
+	18: "Small Bounty Contract",
+	19: "Medium Bounty Contract",
+	20: "Large Bounty Contract",
 };
